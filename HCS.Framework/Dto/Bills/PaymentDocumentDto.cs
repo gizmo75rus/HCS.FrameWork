d5938543eef46f1b84e03c66b34e1c3c87577c8c
Denis Ilyushchenko <iluschenko.d.g@hotmail.com>
2018-04-18 14:13:05 +0300

    добавлены dto и requestBuilder для платежных документов

---
+++ HCS.Framework/Dto/Bills/PaymentDocumentDto.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HCS.Framework.Base;
+
+namespace HCS.Framework.Dto.Bills
+{
+    public class PaymentDataDto : BaseDto {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public bool HasSpecialKP { get; set; }
+        public PaymentRequisites Requisites { get; set; }
+        public List<PaymentDocumentDto> Documents { get; set; }
+        public PaymentDataDto()
+        {
+            Documents = new List<PaymentDocumentDto>();
+        }
+    }
+
+    public class PaymentDocumentDto: BaseDto
+    {
+        /// <summary>
+        /// Требуется отзвать ПД
+        /// </summary>
+        public bool Revoke { get; set; }
+
+        /// <summary>
+        /// Идентификатор ЛС
+        /// </summary>
+        public string AccountGUID { get; set; }
+
+        /// <summary>
+        /// Номер ПД
+        /// </summary>
+        public string PaymentDocumentNumber { get; set; }
+
+        /// <summary>
+        /// Общая площадь помещения
+        /// </summary>
+        public decimal TotalSquare { get; set; }
+
+        /// <summary>
+        /// Начисления по услугам
+        /// </summary>
+        public List<ChargeInfo> ChargesInfos { get; set; }
+
+        /// <summary>
+        /// Кап. ремонт
+        /// </summary>
+        public List<CapitalRepair> CapitalRepairs { get; set; }
+
+        /// <summary>
+        /// Пени
+        /// </summary>
+        public List<Peni> Penies { get; set; }
+
+        /// <summary>
+        /// Задолженности по услугам
+        /// </summary>
+        public List<ChargeDept> ChargesDept { get; set; }
+
+
+        #region  Справочная информация
+
+        /// <summary>
+        /// аванс на начало расчетного периода, руб. 	PrePayment
+        /// </summary>
+        public decimal PrePaymentPeriod { get; set; }
+
+        /// <summary>
+        /// Задолженность за предыдущие периоды
+        /// </summary>
+        public decimal DebtPreviousPeriods { get; set; }
+
+        /// <summary>
+        /// Сумма последнего платежа
+        /// </summary>
+        public decimal Pay { get; set; }
+
+        /// <summary>
+        /// Дата последей поступившей оплаты
+        /// </summary>
+        public DateTime? DateOfLastPay { get; set; }
+
+        #endregion
+
+        public PaymentDocumentDto()
+        {
+            ChargesInfos = new List<ChargeInfo>();
+            CapitalRepairs = new List<CapitalRepair>();
+            Penies = new List<Peni>();
+            ChargesDept = new List<ChargeDept>();
+        }
+
+
+    }
+
+    /// <summary>
+    /// Платежные реквизиты
+    /// </summary>
+    public class PaymentRequisites
+    {
+        public PaymentRequisites()
+        {
+            _RequsiteKEY = Guid.NewGuid().ToString().ToLower();
+        }
+
+        /// <summary>
+        /// БИК Банка
+        /// </summary>
+        public string BIK { get; set; }
+
+        /// <summary>
+        /// Расчетный счет
+        /// </summary>
+        public string BankAccountNumber { get; set; }
+
+        /// <summary>
+        /// Ключь ПД реквизита
+        /// </summary>
+        public string RequsiteKEY { get { return _RequsiteKEY; } }
+
+        private string _RequsiteKEY;
+
+
+    }
+
+
+    /// <summary>
+    /// Начисление по услуге
+    /// </summary>
+    public class ChargeInfo : BaseCharge
+    {
+        public int SubListID { get; set; }
+
+        /// <summary>
+        /// Способ определения объема потребления услуги
+        /// </summary>
+        public DeterminingMethod DeterminingMethod { get; set; }
+
+        /// <summary>
+        /// Тип объема 
+        /// </summary>
+        public VolumeType VolumeType { get; set; }
+
+        /// <summary>
+        /// Объем потребленной услуги
+        /// </summary>
+        public decimal Value { get; set; }
+
+
+        /// <summary>
+        /// Тариф
+        /// </summary>
+        public decimal Rate { get; set; }
+
+        /// <summary>
+        /// Перерасчеты
+        /// </summary>
+        public decimal ReCalc { get; set; }
+
+        /// <summary>
+        /// Итого к оплате
+        /// </summary>
+        public decimal Total { get; set; }
+
+        /// <summary>
+        /// Итого к оплате за период
+        /// </summary>
+        public decimal PeriodTotal { get; set; }
+
+        /// <summary>
+        /// Под услуги для ком.ресурсов на ОДН
+        /// </summary>
+        public List<ChargeInfo> SubCharges { get; set; }
+    }
+
+    /// <summary>
+    /// Начисления за кап.ремонт
+    /// </summary>
+    public class CapitalRepair : BaseCharge
+    {
+        /// <summary>
+        /// Размер взноса за кв.м.
+        /// </summary>
+        public decimal Contribution { get; set; }
+
+        /// <summary>
+        /// Начислено за период (без. перерасчетов)
+        /// </summary>
+        public decimal AccountingPeriodTotal { get; set; }
+
+        /// <summary>
+        /// Перерасчет, корректировка
+        /// </summary>
+        public decimal Recalcultion { get; set; }
+
+        /// <summary>
+        /// Льготы, субсидии, скидки
+        /// </summary>
+        public decimal Discount { get; set; }
+
+        /// <summary>
+        /// Итого к оплате
+        /// </summary>
+        public decimal Total { get; set; }
+    }
+
+    /// <summary>
+    /// Пеня
+    /// </summary>
+    public class Peni : BaseCharge
+    {
+        /// <summary>
+        /// Основание
+        /// </summary>
+        public string Clause { get; set; }
+
+        /// <summary>
+        /// Итого начислено
+        /// </summary>
+        public decimal Total { get; set; }
+    }
+
+    /// <summary>
+    /// Задолженность по услуге
+    /// </summary>
+    public class ChargeDept : BaseCharge
+    {
+
+        /// <summary>
+        /// Месяц
+        /// </summary>
+        public int Month { get; set; }
+
+        /// <summary>
+        /// Год
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Итого к оплате за ресчетный период
+        /// </summary>
+        public decimal TotalDebt { get; set; }
+    }
+
+    #region Base
+
+    public class BaseCharge :NsiRef
+    {
+        public ServiceType ServiceType { get; set; }
+    }
+
+    /// <summary>
+    /// Тип услуг
+    /// </summary>
+    public enum ServiceType
+    {
+        /// <summary>
+        /// Жилищная
+        /// </summary>
+        HousingService = 0,
+
+        /// <summary>
+        /// Муниципальная
+        /// </summary>
+        MunicipalSerive = 1,
+
+        /// <summary>
+        /// Дополнительная
+        /// </summary>
+        AdditionalService = 2,
+
+        /// <summary>
+        /// Ком ресурсы на ОДН
+        /// </summary>
+        HouseResource = 4,
+
+        /// <summary>
+        /// Пени
+        /// </summary>
+        Peni = 5,
+
+
+        /// <summary>
+        /// кап. ремонт
+        /// </summary>
+        CapitalRepair = 6
+
+
+
+    }
+
+    /// <summary>
+    /// Тип объема
+    /// </summary>
+    public enum VolumeType
+    {
+        /// <summary>
+        /// Индивидуальная
+        /// </summary>
+        Individual,
+
+        /// <summary>
+        /// ОДН
+        /// </summary>
+        Сollective
+    }
+
+    /// <summary>
+    /// Способ определения объема потребления услуги
+    /// </summary>
+    public enum DeterminingMethod
+    {
+
+        /// <summary>
+        /// Норматив
+        /// </summary>
+        Norm = 0,
+        /// <summary>
+        /// Прибор учета
+        /// </summary>
+        Metering = 1,
+
+        /// <summary>
+        /// Другое
+        /// </summary>
+        Other = 2
+    }
+    #endregion
+
+}
---
+++ HCS.Framework/RequestBuilders/Bills/ImportPaymentDocument.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HCS.Service.Async.Bills.v11_10_0_13;
+using HCS.Framework.Interfaces;
+using HCS.Framework.Dto.Bills;
+using HCS.Framework.Enums;
+
+namespace HCS.Framework.RequestBuilders.Bills
+{
+    public class ImportPaymentDocument : BaseBuilder, IRequestBuilder<importPaymentDocumentDataRequest, PaymentDataDto>
+    {
+        public importPaymentDocumentDataRequest Build(BuilderOption option, IEnumerable<PaymentDataDto> data)
+        {
+            if (data.Count() > 1)
+                throw new ArgumentOutOfRangeException("Превышено максимальное колличество объектов PaymentDataDto ожидался 1");
+
+            var dto = data.FirstOrDefault();
+            if (dto.Documents?.Count() > LIMIT_BY_REQUEST)
+                throw new ArgumentOutOfRangeException("Превышено максималое кол-во объектов PaymentDocumentDto для одного запроса");
+
+            var documents = new List<object>() {
+                true, //<<------- ConfirmAmountsCorrect
+                dto.Month,
+                dto.Year,
+                PaymentInformation(dto.Requisites)
+            };
+
+            
+            switch (option.Command) {
+                case RequestCMD.Create:
+                    documents.AddRange(dto.Documents.Select(doc => PaymentDocument(dto.HasSpecialKP, doc, dto.Requisites.RequsiteKEY)));
+                    break;
+                case RequestCMD.Delete:
+                    documents.AddRange(dto.Documents.Select(doc => RevokeDocument(doc)));
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return new importPaymentDocumentDataRequest {
+                RequestHeader = Create<RequestHeader>(option.IsOperator, option.Get(ParametrType.OrgPPAGUID)),
+                importPaymentDocumentRequest = new importPaymentDocumentRequest {
+                    Id = RequestID,
+                    Items = documents.ToArray()
+                }
+            };
+        }
+
+        /// <summary>
+        /// Реквизиты ПД
+        /// </summary>
+        /// <param name="requisites"></param>
+        /// <returns></returns>
+        importPaymentDocumentRequestPaymentInformation PaymentInformation(PaymentRequisites requisites)
+        {
+            return new importPaymentDocumentRequestPaymentInformation {
+                TransportGUID = requisites.RequsiteKEY,
+                BankBIK = requisites.BIK,
+                operatingAccountNumber = requisites.BankAccountNumber
+            };
+        }
+
+        /// <summary>
+        /// Платежный документ
+        /// </summary>
+        /// <returns></returns>
+        private importPaymentDocumentRequestPaymentDocument PaymentDocument(bool usedSpecialCapRepair, PaymentDocumentDto dto, string paymentInformationKey)
+        {
+            var document = new importPaymentDocumentRequestPaymentDocument {
+                TransportGUID = dto.TransportGuid,
+                AccountGuid = dto.AccountGUID,
+
+                Item = true,
+                ItemElementName = ItemChoiceType4.Expose,
+
+                PaymentInformationKey = paymentInformationKey,
+
+                // Авансы
+                AdvanceBllingPeriodSpecified = false,
+
+                //Долг
+                DebtPreviousPeriodsSpecified = false,
+
+                //Итог к оплате с учетом рассрочек и.т.п.
+                totalPiecemealPaymentSumSpecified = false,
+                TotalByPenaltiesAndCourtCostsSpecified = false,
+                TotalPayableByChargeInfoSpecified = false,
+                TotalPayableByPDWithDebtAndAdvanceSpecified = false,
+                TotalPayableByPDSpecified = false,
+
+                PaidCashSpecified = false,
+                DateOfLastReceivedPaymentSpecified = false
+
+            };
+
+            if (dto.DebtPreviousPeriods > 0) {
+                document.DebtPreviousPeriodsSpecified = true;
+                document.DebtPreviousPeriods = dto.DebtPreviousPeriods;
+            }
+
+            if (dto.DateOfLastPay.HasValue) {
+                document.PaidCashSpecified = true;
+                document.PaidCash = dto.Pay;
+                document.DateOfLastReceivedPaymentSpecified = true;
+                document.DateOfLastReceivedPayment = dto.DateOfLastPay.Value;
+            }
+
+            var charges = new List<object>();
+
+            if (dto.ChargesInfos.Any()) {
+
+                charges.AddRange(dto.ChargesInfos.Select(charge => ChargeInfo(charge)).ToArray());
+
+                // в случае наличия спец.счета кап.ремонта
+                if (usedSpecialCapRepair && dto.CapitalRepairs?.Count() == 0)
+                    charges.Add(new CapitalRepairImportType { });
+            };
+
+            if (dto.CapitalRepairs.Any()) {
+
+                charges.AddRange(dto.CapitalRepairs.Select(x => new CapitalRepairImportType {
+                    Contribution = x.Contribution,
+                    AccountingPeriodTotal = x.AccountingPeriodTotal,
+                    MoneyRecalculation = x.Recalcultion,
+                    TotalPayable = x.Total
+                }).ToArray());
+            }
+
+            if (dto.Penies.Any()) {
+                charges.Add(new PaymentDocumentTypePenaltiesAndCourtCosts {
+                    Cause = dto.Penies.FirstOrDefault().Clause,
+                    ServiceType = dto.Penies.Select(x => new nsiRef { Code = x.Code, GUID = x.Guid }).FirstOrDefault(),
+                    TotalPayable = dto.Penies.Sum(x => x.Total)
+                });
+
+            }
+
+            document.Items = charges.ToArray();
+            return document;
+        }
+
+        /// <summary>
+        /// Отзыв платежного документа
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        private importPaymentDocumentRequestWithdrawPaymentDocument RevokeDocument(PaymentDocumentDto dto)
+        {
+            var document = new importPaymentDocumentRequestWithdrawPaymentDocument {
+                TransportGUID = dto.TransportGuid,
+                PaymentDocumentID = dto.UniqueNumber
+            };
+            return document;
+        }
+
+
+        /// <summary>
+        /// Начисление по услуги
+        /// </summary>
+        /// <returns></returns>
+        private PaymentDocumentTypeChargeInfo ChargeInfo(ChargeInfo charge)
+        {
+            var result = new PaymentDocumentTypeChargeInfo();
+            switch (charge.ServiceType) {
+                // Жилищные услуги
+                case ServiceType.HousingService:
+                    result.Item = new PDServiceChargeTypeHousingService {
+                        ServiceType = new nsiRef { Code = charge.Code, GUID = charge.Guid },
+                        Rate = charge.Rate,
+                        TotalPayable = charge.Total,
+                        AccountingPeriodTotal = charge.PeriodTotal,
+
+                        ServiceCharge = new ServiceChargeImportType {
+                            MoneyDiscountSpecified = false,
+                            MoneyRecalculationSpecified = true,
+                            MoneyRecalculation = charge.ReCalc,
+                        },
+
+                        MunicipalResource = charge.SubCharges != null ? charge.SubCharges.Select(x => new PDServiceChargeTypeHousingServiceMunicipalResource {
+                            ServiceType = new nsiRef { Code = x.Code, GUID = x.Guid },
+                            Rate = x.Rate,
+                            MunicipalServiceCommunalConsumptionPayable = x.Total,
+                            AccountingPeriodTotalSpecified = true,
+                            AccountingPeriodTotal = x.PeriodTotal,
+                            Consumption = new PDServiceChargeTypeHousingServiceMunicipalResourceConsumption {
+                                Volume = new PDServiceChargeTypeHousingServiceMunicipalResourceConsumptionVolume {
+                                    Value = charge.Value,
+                                    determiningMethodSpecified = true,
+                                    determiningMethod = PDServiceChargeTypeHousingServiceMunicipalResourceConsumptionVolumeDeterminingMethod.N,
+                                    typeSpecified = true,
+                                    type = PDServiceChargeTypeHousingServiceMunicipalResourceConsumptionVolumeType.O
+                                }
+                            }
+                        }).ToArray() : new PDServiceChargeTypeHousingServiceMunicipalResource[] { }
+
+                    };
+
+                    break;
+                case ServiceType.AdditionalService:
+                    // Дополнительные услуги
+                    result.Item = new PDServiceChargeTypeAdditionalService {
+                        //Услуга
+                        ServiceType = new nsiRef { Code = charge.Code, GUID = charge.Guid },
+
+                        //Тариф
+                        Rate = charge.Rate,
+
+                        //Всего
+                        TotalPayable = charge.Total,
+
+                        AccountingPeriodTotal = charge.PeriodTotal,
+
+                        ServiceCharge = new ServiceChargeImportType {
+                            MoneyDiscountSpecified = false,
+                            MoneyRecalculationSpecified = true,
+                            MoneyRecalculation = charge.ReCalc,
+                        },
+
+                        //Порядок расчетов
+                        CalcExplanation = "Автоматический"
+
+                    };
+                    break;
+                case ServiceType.MunicipalSerive:
+                    //Коммунальные услуги
+                    result.Item = new PDServiceChargeTypeMunicipalService {
+                        //Услуга
+                        ServiceType = new nsiRef { Code = charge.Code, GUID = charge.Guid },
+
+                        //Тариф
+                        Rate = charge.Rate,
+
+                        //Всего
+                        TotalPayable = charge.Total,
+
+                        //Итого с учетом перерасчетов
+                        AccountingPeriodTotal = charge.PeriodTotal,
+
+                        //Порядок расчетов
+                        CalcExplanation = "Автоматический",
+
+
+                        // К оплате за потребление комм услуги ОДН
+                        MunicipalServiceCommunalConsumptionPayableSpecified = false,
+
+                        // К оплате за потребление комм услуги индв.
+                        MunicipalServiceIndividualConsumptionPayableSpecified = false,
+
+
+                        // Перерасчеты
+                        ServiceCharge = new ServiceChargeImportType {
+                            MoneyRecalculationSpecified = true,
+                            MoneyRecalculation = charge.ReCalc
+                        },
+
+                        // Объем услуг
+                        Consumption = new PDServiceChargeTypeMunicipalServiceVolume[] { new PDServiceChargeTypeMunicipalServiceVolume {
+
+                            determiningMethodSpecified = true,
+                            //Способ определения объема: M - счетчик, N - Норматив, O - Другое
+
+                            determiningMethod = (PDServiceChargeTypeMunicipalServiceVolumeDeterminingMethod)charge.DeterminingMethod,
+
+                            typeSpecified = true,
+                            //Тип усуги: I - индивидуальная, O - Общедомовая
+                            type = (PDServiceChargeTypeMunicipalServiceVolumeType)charge.VolumeType,
+
+                            //Объем
+                            Value = charge.Value,
+                        } },
+
+
+                        ServiceInformation = new ServiceInformation {
+                            individualConsumptionCurrentValueSpecified = true,
+                            individualConsumptionCurrentValue = 5
+                        }
+
+                    };
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
