0500dbd95bc89f17d2479306e8c8465427e1d0cb
Denis Ilyushchenko <iluschenko.d.g@hotmail.com>
2018-04-18 15:02:01 +0300

    для OrderExecution добавлены Dto и RequestBuilder

---
+++ HCS.Framework/Dto/Payment/OrderDataDto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HCS.Framework.Base;
+
+namespace HCS.Framework.Dto.Payment
+{
+    //DTO для «Извещение о принятии к исполнению распоряжения», размещаемых исполнителем
+    public class OrderDataDto : BaseDto
+    {
+        public IEnumerable<OrderExecutionDto> Orders { get; set; }
+    }
+    public class OrderExecutionDto:BaseDto
+    {
+        /// <summary>
+        /// Идентификатор оплаты от ГИС
+        /// </summary>
+        public string OrderGuid { get; set; }
+
+        /// <summary>
+        /// Тип оплаты
+        /// </summary>
+        public PaymentType Type { get; set; }
+
+        /// <summary>
+        /// Дата внесения
+        /// </summary>
+        public DateTime OrderDate { get; set; }
+
+        /// <summary>
+        /// Оплата за период
+        /// </summary>
+        public OrderPeriod Period { get; set; }
+
+        /// <summary>
+        /// Идентификатор Жку
+        /// </summary>
+        public string ServiceID { get; set; }
+
+        /// <summary>
+        /// Номер л/с
+        /// </summary>
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// ЕЛС
+        /// </summary>
+        public string AccountUniqueNumber { get; set; }
+
+        /// <summary>
+        /// Идентификатор ПД
+        /// </summary>
+        public string PaymentDocumentID { get; set; }
+
+        /// <summary>
+        /// Сумма платежа
+        /// </summary>
+        public decimal Amount { get; set; }
+    }
+
+    public class OrderPeriod
+    {
+        public int Month { get; set; }
+        public short Year { get; set; }
+    }
+
+    /// <summary>
+    /// Тип оплаты
+    /// </summary>
+    public enum PaymentType
+    {
+        /// <summary>
+        /// Банк
+        /// </summary>
+        Bank = 0,
+
+        /// <summary>
+        /// Касса
+        /// </summary>
+        CashBox = 1
+    }
+}
---
+++ HCS.Framework/RequestBuilders/Payments/ImportSupplierOrderExecution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HCS.Service.Async.Payment.v11_10_0_13;
+using HCS.Framework.Interfaces;
+using HCS.Framework.Enums;
+using HCS.Framework.Dto.Payment;
+
+namespace HCS.Framework.RequestBuilders.Payments
+{
+    public class ImportSupplierOrderExecution : BaseBuilder, IRequestBuilder<importSupplierNotificationsOfOrderExecutionRequest1, OrderDataDto>
+    {
+        public importSupplierNotificationsOfOrderExecutionRequest1 Build(BuilderOption option, IEnumerable<OrderDataDto> data)
+        {
+            var dto = data.FirstOrDefault();
+            return new importSupplierNotificationsOfOrderExecutionRequest1 {
+                RequestHeader = Create<RequestHeader>(option.IsOperator,option.Get(ParametrType.OrgPPAGUID)),
+                importSupplierNotificationsOfOrderExecutionRequest = new importSupplierNotificationsOfOrderExecutionRequest {
+                    Id = RequestID,
+                    SupplierNotificationOfOrderExecution = getOrders(dto.Orders)
+                }
+            };
+        }
+
+        importSupplierNotificationsOfOrderExecutionRequestSupplierNotificationOfOrderExecution[] getOrders(IEnumerable<OrderExecutionDto> data)
+        {
+            var value = new List<importSupplierNotificationsOfOrderExecutionRequestSupplierNotificationOfOrderExecution>();
+            data.ToList().ForEach(dto => {
+                value.Add(new importSupplierNotificationsOfOrderExecutionRequestSupplierNotificationOfOrderExecution {
+                    TransportGUID = dto.TransportGuid,
+                    Amount = dto.Amount,
+                    ItemElementName = ItemChoiceType1.ServiceID,
+                    Item = dto.ServiceID,
+                    OrderDate = dto.OrderDate,
+                });
+            });
+
+            return value.ToArray();
+        }
+    }
+}
